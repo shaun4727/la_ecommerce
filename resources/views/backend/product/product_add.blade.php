04c9077c01bcbc9ee0bb99588c04baab6bcabc0f
shaun4727 <107396795+shaun4727@users.noreply.github.com>
2023-10-24 08:11:46 +0600

    Update product_add.blade.php

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -246,12 +246,12 @@
                         <div class="form-group">
                             <div class="controls">
                                 <fieldset>
-                                    <input type="checkbox" id="checkbox_2"  value="1" name="special_offer">
-                                    <label for="checkbox_2">Special Offer</label>
+                                    <input type="checkbox" id="checkbox_5"  value="1" name="special_offer">
+                                    <label for="checkbox_5">Special Offer</label>
                                 </fieldset>
                                 <fieldset>
-                                    <input type="checkbox" id="checkbox_3" value="1" name="special_deals">
-                                    <label for="checkbox_3">Special Deals</label>
+                                    <input type="checkbox" id="checkbox_6" value="1" name="special_deals">
+                                    <label for="checkbox_6">Special Deals</label>
                                 </fieldset>
                             </div>
                         </div>
